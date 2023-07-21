c47387cf9b9fdfbac003038402142ba56585de59
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-20 23:54:10 -0300

    init

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+const title = document.querySelector('title')
+const spanGems = document.querySelector('span#gems')
+const spanClicks = document.querySelector('span#gems-per-click')
+const imageGem = document.querySelector('img#image')
+const btPoints = document.querySelector('button#points-button')
+const spanPrice = document.querySelector("span#button-price")
+
+
+let gems = 0
+let clicks = 1
+let buttonPrice = 10
+
+// Função para aumentar os pontos ao clicar na imagem
+function increasePoints() {
+  gems += clicks
+}
+
+function updateclicks(){
+    clicks++
+    gems -= buttonPrice
+
+    buttonPrice *= 3.5;
+    spanPrice.innerHTML = buttonPrice
+}
+
+const loop = setInterval( () => {
+    spanGems.innerHTML = gems
+    spanClicks.innerHTML = clicks
+
+    if (gems > 0){
+        title.innerHTML = `Gemas: ${gems}`
+    }
+    
+    if (gems >= buttonPrice){
+        btPoints.disabled = false
+    } else {
+        btPoints.disabled = true
+    }
+})
+
+imageGem.addEventListener('click', increasePoints);
+btPoints.addEventListener('click', updateclicks);
