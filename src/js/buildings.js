cb19f0b673f6645d7cf14cf8e061f749b79466a3
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-22 19:44:18 -0300

    click-upgrade

--- src/js/buildings.js
+++ src/js/buildings.js
@@ -1,6 +1,7 @@
+import { buildings_upgrades } from "./script.js"
+
 const buildings = [
     {
-        id: 0,
         description : "Click +0.1",
         image_src : "src/images/icons/store-items/cursor.png",
         show_price : "5 gems",
@@ -10,7 +11,6 @@ const buildings = [
         func: 1
     },
     {
-        id: 1,
         description : "Pickaxe +0.1/s",
         image_src : "src/images/icons/store-items/pickaxe.png",
         show_price : "15 gems",
@@ -20,7 +20,6 @@ const buildings = [
         func: 2
     },
     {
-        id: 1,
         description : "Miner +1/s",
         image_src : "src/images/icons/store-items/miner.png",
         show_price : "100 gems",
@@ -30,10 +29,53 @@ const buildings = [
         func: 2
     },
 ]
-export function buildings_upgrades(gems, ){
 
+export function builds(){
+    return buildings
 }
 
-export function return_buildings(){
-    return buildings
+export function createButtons(show){
+    const store = document.querySelector("div.store")
+
+    for (let index = 0; index < show; index++){
+    
+        const created_button = document.createElement("button")
+        created_button.setAttribute("id", `button-${index}`)
+        created_button.setAttribute("class", "btBuilds")
+
+        const itemCont = document.createElement("h1") 
+        itemCont.setAttribute("class", "cont-item")
+        itemCont.setAttribute("id", `item-cont-${index}`)
+        itemCont.textContent = buildings[index].cont_item
+
+        // div principal
+        const div_img_info = document.createElement("div")
+        div_img_info.setAttribute("class", "button-img-info")
+
+        const img = document.createElement("img")
+        img.src = buildings[index].image_src
+        img.alt = "img-build"
+
+        //div interna
+        const div_desc = document.createElement("div")
+        div_desc.setAttribute("class", "btBuilds-info")
+
+        const h1_desc = document.createElement("h1")
+        h1_desc.textContent = buildings[index].description
+
+        const pPrice = document.createElement("p")
+        pPrice.setAttribute("id", `price-${index}`)
+        pPrice.textContent = buildings[index].show_price
+        
+        div_desc.append(h1_desc, pPrice)
+        div_img_info.append(img, div_desc)
+        created_button.append(div_img_info, itemCont)
+        store.append(created_button)
+
+        created_button.addEventListener("click", () => {
+            buildings_upgrades(buildings[index].func, index)
+        })
+
+        created_button.disabled = true
+    }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
-import { createButtons } from "./store.js"
-createButtons(3)
+import { createButtons, builds } from "./buildings.js"
+
+let show = 3
+createButtons(show)
+
+const buildings = builds()
 
 const title = document.querySelector('title')
 const spanGems = document.querySelector('span#gems')
@@ -7,77 +11,84 @@ const spanClicks = document.querySelector('span#gems-per-click')
 const spanClicksPS = document.querySelector('span#gems-per-second')
 const imageGem = document.querySelector('img#image')
 
-// const btPoints = document.querySelector('button#points-button')
-// const pPrice = document.querySelector("p#button-price")
-// const itemCont = document.querySelector("h1#cont-item")
-
-// const pickaxeButton = document.querySelector("button#pickaxe-button")
-// const pPrice2 = document.querySelector("p#button-price2")
-// const itemCont2 = document.querySelector("h1#cont-item-2")
-
-let show = 1
 let gems = 0
 let gemsPerSecond = 0
-// botão 1
 let clicks = 1
-let contitem = 0
-let buttonPriceBase = 5
-let buttonPrice = buttonPriceBase * Math.pow(1.2, contitem)
-
-//botao 2
-let timer = 0
-let buttonPriceBase2 = 15
-let buttonPrice2 = buttonPriceBase2 * Math.pow(1.2, contitem)
-let contitem2 = 0
 
 // Função para aumentar os pontos ao clicar na imagem
 function increasePoints() {
-  gems += clicks
-  gems = Number(gems.toFixed(1))
-}
-
-function updateclicks(){
-    clicks += 0.1
-    clicks = Number(clicks.toFixed(1))
-
-    contitem++
-    itemCont.innerHTML = contitem
-
-    gems -= buttonPrice
+    gems += clicks
     gems = Number(gems.toFixed(1))
-
-    buttonPrice = buttonPriceBase * Math.pow(1.5, contitem)
-    buttonPrice = Math.floor(buttonPrice)
-    pPrice.innerHTML = `${buttonPrice} gems`
 }
+  
 
-function clickspersecond(){
-    contitem2++
-    itemCont2.innerHTML = contitem2
+export function buildings_upgrades(upgrade = Number, index = Number){
+    switch (upgrade){
+        case 1:
+            const pPrice = document.querySelector(`p#price-${index}`)
+            const cont_text = document.querySelector(`h1#item-cont-${index}`)
 
-    timer += 100
-    gemsPerSecond += 0.1
+            clicks += 0.1
+            clicks = Number(clicks.toFixed(1))
 
-    spanClicksPS.innerHTML = gemsPerSecond
+            buildings[index].cont_item++
+            cont_text.textContent = buildings[index].cont_item
 
-    buttonPrice2 = buttonPriceBase2 * Math.pow(1.5, contitem2)
-    buttonPrice2 = Math.floor(buttonPrice2)
-    pPrice2.innerHTML = `${buttonPrice2} gems`
+            gems -= buildings[index].price
+            gems = Number(gems.toFixed(1))
 
+            buildings[index].price = Math.floor(
+                buildings[index].init_price * Math.pow(1.5, buildings[index].cont_item)
+                )
+            pPrice.textContent = `${buildings[index].price} gems`
+            
+            break
+        case 2:
+            console.log("per second")
+            break
+    }
 }
 
 const loop = setInterval( () => {
-   
     spanGems.innerHTML = gems
     spanClicks.innerHTML = clicks
 
     if (gems > 0){
-        title.innerHTML = `${gems} gems - Gem Clicker`
+        title.textContent = `${gems} gems - Gem Clicker`
     } else {
-        title.innerHTML = "Gem Clicker"
+        title.textContent = "Gem Clicker"
+    }
+
+    // checa se o player tem gemas suficiente para cada upgrade
+    for (let index = 0; index < show; index++){
+        const button = document.querySelector(`button#button-${index}`)
+
+        if (gems >= buildings[index].price){
+            button.disabled = false
+        } else {
+            button.disabled = true
+        }
     }
     
-    // if (gems >= buttonPrice){
+})
+imageGem.addEventListener('click', increasePoints);
+
+// function clickspersecond(){
+//     contitem2++
+//     itemCont2.innerHTML = contitem2
+
+//     timer += 100
+//     gemsPerSecond += 0.1
+
+//     spanClicksPS.innerHTML = gemsPerSecond
+
+//     buttonPrice2 = buttonPriceBase2 * Math.pow(1.5, contitem2)
+//     buttonPrice2 = Math.floor(buttonPrice2)
+//     pPrice2.innerHTML = `${buttonPrice2} gems`
+
+// }
+
+// if (gems >= buttonPrice){
     //     btPoints.disabled = false
     // } else {
     //     btPoints.disabled = true
@@ -88,9 +99,3 @@ const loop = setInterval( () => {
     // } else {
     //     pickaxeButton.disabled = true
     // }
-    
-})
-
-imageGem.addEventListener('click', increasePoints);
-// btPoints.addEventListener('click', updateclicks);
-// pickaxeButton.addEventListener('click', clickspersecond)
