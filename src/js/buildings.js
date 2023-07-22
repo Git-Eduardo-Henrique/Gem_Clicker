940c423a4822903d84ad1e699917d78ccf0e39a0
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-07-22 02:18:09 -0300

    auto-click-v1

---
+++ src/js/buildings.js
@@ -0,0 +1,17 @@
+// {/* <button id="points-button" class="btpoints">
+//                 <div class="button-img-info">
+//                     <img src="src/images/icons/store-items/miner.png" alt="add-click">
+//                     <div class="btpoints-info">
+//                         <h1>Miner +1/s (not avaliable)</h1>
+//                         <p id="button-price">100 gems</p> 
+//                     </div>
+//                 </div>
+//                 <h1 id="cont-item" class="cont-item">0</h1>
+//             </button> */}
+
+
+const buildings = [
+    {
+        
+    }
+]
--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,31 @@
 const title = document.querySelector('title')
 const spanGems = document.querySelector('span#gems')
 const spanClicks = document.querySelector('span#gems-per-click')
+const spanClicksPS = document.querySelector('span#gems-per-second')
 const imageGem = document.querySelector('img#image')
+
 const btPoints = document.querySelector('button#points-button')
 const pPrice = document.querySelector("p#button-price")
 const itemCont = document.querySelector("h1#cont-item")
 
+const pickaxeButton = document.querySelector("button#pickaxe-button")
+const pPrice2 = document.querySelector("p#button-price2")
+const itemCont2 = document.querySelector("h1#cont-item-2")
+
 
 let gems = 0
+let gemsPerSecond = 0
 // botão 1
 let clicks = 1
 let contitem = 0
 let buttonPriceBase = 10
 let buttonPrice = buttonPriceBase * Math.pow(1.2, contitem)
 
+//botao 2
+let timer = 0
+let buttonPriceBase2 = 15
+let buttonPrice2 = buttonPriceBase2 * Math.pow(1.2, contitem)
+let contitem2 = 0
 
 // Função para aumentar os pontos ao clicar na imagem
 function increasePoints() {
@@ -26,6 +38,8 @@ function updateclicks(){
     clicks = Number(clicks.toFixed(1))
 
     contitem++
+    itemCont.innerHTML = contitem
+
     gems -= buttonPrice
     gems = Number(gems.toFixed(1))
 
@@ -34,10 +48,24 @@ function updateclicks(){
     pPrice.innerHTML = `${buttonPrice} gems`
 }
 
+function clickspersecond(){
+    contitem2++
+    itemCont2.innerHTML = contitem2
+
+    timer += 100
+    gemsPerSecond += 0.1
+
+    spanClicksPS.innerHTML = gemsPerSecond
+
+    buttonPrice2 = buttonPriceBase2 * Math.pow(1.5, contitem2)
+    buttonPrice2 = Math.floor(buttonPrice2)
+    pPrice2.innerHTML = `${buttonPrice2} gems`
+
+}
+
 const loop = setInterval( () => {
     spanGems.innerHTML = gems
     spanClicks.innerHTML = clicks
-    itemCont.innerHTML = contitem
 
     if (gems > 0){
         title.innerHTML = `${gems} gems - Gem Clicker`
@@ -50,7 +78,15 @@ const loop = setInterval( () => {
     } else {
         btPoints.disabled = true
     }
+
+    if (gems >= buttonPrice2){
+        pickaxeButton.disabled = false
+    } else {
+        pickaxeButton.disabled = true
+    }
+    
 })
 
 imageGem.addEventListener('click', increasePoints);
 btPoints.addEventListener('click', updateclicks);
+pickaxeButton.addEventListener('click', clickspersecond)
